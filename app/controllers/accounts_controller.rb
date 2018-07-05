88b5fc8a1b88ab28e288d1fb3ac7f7bbe33b18db
S-zebra <shimashimazebra13@gmail.com>
2018-07-05 23:51:46 +0900

    アカウント登録機能の作成

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,13 @@
 class AccountsController < ApplicationController
   def new
-    @new_account=Account.new()
+    @new_account = Account.new
+  end
+
+  def create
+    account = params.require(:account).permit(:student_id, :login_password) #for strong parameters
+    @new_account = Account.new(student_id: account[:student_id], login_password: account[:login_password])
+    if @new_account.save
+      render plain: "Account created."
+    end
   end
 end
--- db/migrate/20180705043808_create_accounts.rb
+++ db/migrate/20180705043808_create_accounts.rb
@@ -1,7 +1,9 @@
 class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
-      
+      t.string :student_id, null: false
+      t.string :login_password, null: false
+      t.string :generated_hash, uniquenss: true
       t.timestamps
     end
   end
