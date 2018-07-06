0d63cf8cb3dd78746c1b5405a450119f850926e4
S-zebra <shimashimazebra13@gmail.com>
2018-07-06 14:49:12 +0900

    account暗号化

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,15 @@
 class Account < ApplicationRecord
+  has_many :tokens
+
+  validates :student_id, :login_password, presence :true, uniqueness: {allow_blank: false}
+  before_save :encrypt
+  
+  SECURE = File.read("#{Rails.root}/config/master.key").chomp
+  CIPHER = "aes-256-cbc"
+
+  def encrypt
+    crypt = ActiveSupport::MessageEncryptor.new(SECURE, CIPHER)
+    self.login_password = crypt.encrypt_and_sign(self.login_password)
+  end
+  
 end
