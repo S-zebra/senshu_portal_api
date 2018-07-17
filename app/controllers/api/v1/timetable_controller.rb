a63a68cc401f7215f7cdf073efcd465da67b1ab1
S-zebra <shimashimazebra13@gmail.com>
2018-07-17 11:07:20 +0900

    timetableの修正

--- app/controllers/api/v1/timetable_controller.rb
+++ app/controllers/api/v1/timetable_controller.rb
@@ -29,20 +29,20 @@ class Api::V1::TimetableController < ApplicationController
         end
         text_nodes = cell.content.split("\n")
         images = cell.css("img")
-        if images.size > 0
-          tn_index = 5
-          cr_index = 6
-        else
-          tn_index = 3
-          cr_index = 4
-        end
+        # if images.size > 0
+        #   tn_index = 5
+        #   cr_index = 6
+        # else
+        tn_index = 3
+        cr_index = 4
+        # end
         lecture = Lecture.new(
           account: @account,
           day_of_week: dow + 1,
           slot: slot - 2,
           lecture_name: cell.css("a").first.content.gsub(/[\[\]]/, ""),
           teacher_name: text_nodes[tn_index].split(/\]/)[1].gsub(/^\s+|\s+$/, ""),
-          classroom_name: text_nodes[cr_index].split(/\(/)[0].gsub(/^\s+|\s+$/, ""),
+          classroom_name: text_nodes[cr_index].gsub(/^\s+|\s+$/, "").split("(")[0].gsub(/\s+$/, ""),
           change_status: images.first ? images.first.attribute("title").value : nil,
         )
         lecture.save!
